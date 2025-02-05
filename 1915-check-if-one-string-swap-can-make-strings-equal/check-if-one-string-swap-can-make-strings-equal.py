dac5c80c883243921c5be9da779f498ab1b41188
Abhay Kumara Sri Krishna Nandiraju <98251146+srikrish2812@users.noreply.github.com>
2025-02-04 19:02:44 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (67.41%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,19 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        """
+        Ideally, the strings should have two differ by only two indices so that
+        a single swap in one of the string makes both the strings equal. Now, we
+        need to find those indices and make sure the letters also match. We do this
+        by:-
+        1. Finding the indices where they are different
+        """
+        indexes = []
+        for i in range(len(s1)):
+            if s1[i]!=s2[i]:
+                indexes.append(i)
+            if len(indexes)>2:
+                return False
+        if len(indexes)==2:
+            i,j = indexes
+            return s1[i]==s2[j] and s2[i]==s1[j]
+        return len(indexes)==0
